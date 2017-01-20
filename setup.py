231f19ea6ac89be02c9ed4cdb88b94b3dad418b7
Vasileios Martos <vasileios.martos@maxiv.lu.se>
2017-01-20 13:18:18 +0100

    debian package files added

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="python-liberaexporter",
+    version="0.1.0",
+    description="Prometheus exporter for libera devices.",
+    author="Vasileios Martos",
+    author_email="vasileios.martos@maxlab.lu.se",
+    license="GPLv3",
+    url="http://www.maxlab.lu.se",
+    packages=['liberaexporter'],
+    entry_points={
+        'console_scripts': [
+            'liberaexporter = liberaexporter:main'
+        ]
+    }
+)
